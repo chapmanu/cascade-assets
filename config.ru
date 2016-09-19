59e215d40f537a6b50cf596da7cde2bd859a7fa6
Tom Atwell <atwell@chapman.edu>
2016-09-19 14:08:09 -0700

    Adds index page with README for static ad landing pages.

--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
 # This file is used by Rack-based servers to start the application.
 
-# Make everything under this dir static for rapid dev of mergency static landing pages.
+# Make everything under this dir static for rapid dev of emergency static ad landing pages.
 use Rack::Static, :urls => ["/interim-ad-landing-pages"]
 
 require ::File.expand_path('../config/environment', __FILE__)
--- interim-ad-landing-pages/download.sh
+++ interim-ad-landing-pages/download.sh
@@ -8,10 +8,12 @@ ASSETS_DIR=$TARGET_DIR/_assets
 
 echo "Will save $SAMPLE_PAGE_URL $TARGET_DIR"
 
+# See http://stackoverflow.com/questions/17412199/export-static-htmlcssjs-from-rails
 wget -v -nH -e robots=off \
      --content-disposition --page-requisites --convert-links --convert-file-only \
      $SAMPLE_PAGE_URL
 
+# From http://superuser.com/a/1019508/641578
 for i in `find $ASSETS_DIR -type f`
 do
     mv $i `echo $i | cut -d? -f1`
