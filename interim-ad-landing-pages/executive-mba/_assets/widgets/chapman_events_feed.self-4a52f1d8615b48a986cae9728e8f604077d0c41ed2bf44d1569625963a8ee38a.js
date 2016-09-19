d3a39b6512a93c7e8fee023ab96f41b94c2375b7
waidelacruz <delacruz@chapman.edu>
2016-09-19 14:19:32 -0700

    Add executive-mba

---
+++ interim-ad-landing-pages/executive-mba/_assets/widgets/chapman_events_feed.self-4a52f1d8615b48a986cae9728e8f604077d0c41ed2bf44d1569625963a8ee38a.js
@@ -0,0 +1,13 @@
+/**
+ * Runs on Document ready
+ */
+
+$(document).ready(function() {
+  $('[data-chapman-events-feed]').each(function() {
+    $(this).chapmanEventsFeed({ 
+      feed_path: $(this).data('chapman-events-feed'),
+      featured_path: $('[data-chapman-featured-event]').data('chapman-featured-event'),
+      per: 4
+    });
+  });
+});
