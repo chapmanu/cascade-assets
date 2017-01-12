360e405b8a6d81d73f2fa20d75a1f6b1fd876e4c
Tom Atwell <atwell@chapman.edu>
2017-01-11 16:19:59 -0800

    Adds Cascade Macros class and stubs out first method. Having second thoughts about this approach.

---
+++ lib/cascade/macros.rb
@@ -0,0 +1,14 @@
+#
+# This class mimics, or parodies, various Velocity macros found in Cascade.
+#
+# TODO: Need to rethink this. Cascade macros function more like partials than they do helper
+# methods.
+#
+module Cascade
+  class Macros
+    # From _cascade/formats/modular/widgets/1 Column/image_with_text
+    def self.outputImageWithText(element)
+      'TODO: Need to rethink this. Macros function more like partials than they do helper methods.'
+    end
+  end
+end
