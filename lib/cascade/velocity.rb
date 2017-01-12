231130278a719e25bdbc327ce4ff915df00bab3c
Tom Atwell <atwell@chapman.edu>
2017-01-11 16:03:53 -0800

    Adds import method for Cascade lib.

--- lib/cascade/velocity.rb
+++ lib/cascade/velocity.rb
@@ -5,16 +5,23 @@
 #
 module Cascade
   class Velocity
+    # This method is syntactic pap. It doesn't do anything functional except mimic the VTL import
+    # method in a view context. Macros should be added to macros.rb.
+    # Usage: Cascade::Velocity.import('/_cascade/formats/helpers.velocity')
+    def self.import(path)
+        path
+    end
+
+    # More syntactic pap. It mimics the VTL set method in a view context.
+    # Usage:
+    def self.set(var)
+        var
+    end
+
     # Simple proof-of-concept method.
     # Usage: <%= Cascade::Velocity.output('<h3>Primary Content Here</h3>') %>
     def self.output(html)
         html.html_safe
-    end
-
-    # This method is syntactic pap. It doesn't do anything functional except mimic the VTL set
-    # method in a view context.
-    def self.set(var)
-        var
     end
   end
 end
