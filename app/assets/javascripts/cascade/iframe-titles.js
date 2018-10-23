3347972c1ed249e63835457c41aca685f19208e3
Nicholas Nadel <nicholasnadel@gmail.com>
2018-10-23 13:46:24 -0700

    working noscript implementation

--- app/assets/javascripts/cascade/iframe-titles.js
+++ app/assets/javascripts/cascade/iframe-titles.js
@@ -5,7 +5,7 @@
       arg = arguments,
       src = this,
       clc = null, // collection
-      lng = 50, // length of time to wait between intervals
+      lng = 5, // length of time to wait between intervals
       ivl = -1, // interval id
       chk = function (ifr) {
         try {
@@ -26,7 +26,6 @@
                 // should be loaded
                 ifr.data("ready", true);
               }
-
               break;
             case "interactive":
               ifr.data("ready", "true");
@@ -41,10 +40,8 @@
           // since we won't be able to access it cross domain
           ifr.data("ready", "true");
         }
-
         return ifr.data("ready") === "true";
       };
-
     if (ifr.length) {
       ifr.each(function () {
         if (!$(this).data("ready")) {
@@ -62,7 +59,6 @@
               }
             }
           });
-
           if (rd) {
             clearInterval(ivl);
             clc = null;
@@ -80,32 +76,38 @@
     return this;
   };
 }(jQuery, document));
-
 $(window).load(function () {
+
   var attr = $(this).attr('title');
   if (typeof attr !== typeof undefined && attr !== false) {
     $("iframe").on('load', function () {
-      $('iframe').attr('title', 'A document using iframe');
+      $('iframe').attr('title', 'Embedded content from external source');
     });
   }
-
   $("iframe").iready(function () {
+    // 1. Replace <noscript></noscript> in order to edit contents
+    $('noscript').replaceWith('<tempscript>' + $('noscript').html() + '</tempscript>')
     var ifr = $("#test");
-    // console.log("iready - Should now be ready.");
+    console.log("iready - Should now be ready.");
     try {
-      // console.log($("#iframe").contents()[0].readyState);
+      console.log($("#iframe").contents()[0].readyState);
     } catch (ignore) {
-      // console.log("cross domain");
+      console.log("cross domain");
+      // 2. If iFrame is missing title, add a generic one
       var attr = $(this).attr('title');
       if (typeof attr !== typeof undefined && attr !== false) {
         $("iframe").on('load', function () {
-          $('iframe').attr('title', 'A document using iframe');
+          $('iframe').attr('title', 'Embedded content from external source');
+          $('tempscript').replaceWith('<noscript>' + $('tempscript').html() + '</noscript>')
+          $("noscript").attr("aria-role", "presentation");
+
         });
       }
     }
   });
-
   try {
-    // console.log("Should not be ready: " + $("#iframe").contents()[0].readyState);
-  } catch (ignore) {}
+    console.log("Should not be ready: " + $("#iframe").contents()[0].readyState);
+  } catch (ignore) { }
 });
+
+
