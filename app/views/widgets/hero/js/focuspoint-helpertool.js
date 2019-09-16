b2b32385928d3cfdc52b950c0137d62f2d10a169
Nicholas Nadel <nicholasnadel@gmail.com>
2019-09-16 13:56:34 -0700

    working focal point on staging

--- app/views/widgets/hero/js/focuspoint-helpertool.js
+++ app/views/widgets/hero/js/focuspoint-helpertool.js
@@ -1,47 +1,51 @@
 $(function () {
-  var draggable = $('.draggable'); //element 
+  if ('.draggable'.length > 0) {
 
-  draggable.on('mousedown', function (e) {
-    var dr = $(this).addClass("drag").css("cursor", "move");
-    height = dr.outerHeight();
-    width = dr.outerWidth();
-    max_left = dr.parent().offset().left + dr.parent().width() - dr.width();
-    max_top = dr.parent().offset().top + dr.parent().height() - dr.height();
-    min_left = dr.parent().offset().left;
-    min_top = dr.parent().offset().top;
+    var draggable = $('.draggable'); //element 
 
-    ypos = dr.offset().top + height - e.pageY,
-      xpos = dr.offset().left + width - e.pageX;
-    $(document.body).on('mousemove', function (e) {
-      var itop = e.pageY + ypos - height;
-      var ileft = e.pageX + xpos - width;
+    draggable.on('mousedown', function (e) {
+      var dr = $(this).addClass("drag").css("cursor", "move");
+      height = dr.outerHeight();
+      width = dr.outerWidth();
+      max_left = dr.parent().offset().left + dr.parent().width() - dr.width();
+      max_top = dr.parent().offset().top + dr.parent().height() - dr.height();
+      min_left = dr.parent().offset().left;
+      min_top = dr.parent().offset().top;
 
-      if (dr.hasClass("drag")) {
-        if (itop <= min_top) {
-          itop = min_top;
+      ypos = dr.offset().top + height - e.pageY,
+        xpos = dr.offset().left + width - e.pageX;
+      $(document.body).on('mousemove', function (e) {
+        var itop = e.pageY + ypos - height;
+        var ileft = e.pageX + xpos - width;
+
+        if (dr.hasClass("drag")) {
+          if (itop <= min_top) {
+            itop = min_top;
+          }
+          if (ileft <= min_left) {
+            ileft = min_left;
+          }
+          if (itop >= max_top) {
+            itop = max_top;
+          }
+          if (ileft >= max_left) {
+            ileft = max_left;
+          }
+          dr.offset({
+            top: itop,
+            left: ileft
+          });
         }
-        if (ileft <= min_left) {
-          ileft = min_left;
-        }
-        if (itop >= max_top) {
-          itop = max_top;
-        }
-        if (ileft >= max_left) {
-          ileft = max_left;
-        }
-        dr.offset({
-          top: itop,
-          left: ileft
-        });
-      }
-    }).on('mouseup', function (e) {
-      dr.removeClass("drag");
+      }).on('mouseup', function (e) {
+        dr.removeClass("drag");
+      });
     });
-  });
+  }
 });
 
 
-$(function() {
-  $( ".focuspoint-wrapper" ).fadeIn( "slow", function() {
-  });
+$(function () {
+  if ( ".focuspoint-wraper".length > 0 ) {
+    $(".focuspoint-wrapper").fadeIn("slow", function () {});
+  }
 });
