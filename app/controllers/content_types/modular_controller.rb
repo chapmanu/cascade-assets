b87c044860e3e35b491fe19ce8526a724d22aefc
Nicholas Nadel <nicholasnadel@gmail.com>
2019-09-17 12:16:50 -0700

    initial structure

--- app/controllers/content_types/modular_controller.rb
+++ app/controllers/content_types/modular_controller.rb
@@ -85,7 +85,7 @@ module ContentTypes
         "PAGE WRAPPER OPEN" => cascade_format("_cascade/formats/modular/page_wrapper_open"),
 
         # TODO: convert these to cascade_format action.
-        "OMNI-NAV" => render_static_partial("widgets/shared/omninav"),
+        "OMNI-NAV" => render_static_partial("omninav_v3/omninav"),
         "NAVIGATION" => render_static_partial(navigation_path),
         "PRIMARY CONTENT" => render_static_one_column_primary_content,
         "GLOBAL FOOTER" => render_static_partial(footer_path),
@@ -123,7 +123,7 @@ module ContentTypes
         "PAGE WRAPPER OPEN" => cascade_format("_cascade/formats/modular/page_wrapper_open"),
 
         # TODO: convert these to cascade_format action.
-        "OMNI-NAV" => render_static_partial("widgets/shared/omninav"),
+        "OMNI-NAV" => render_static_partial("omninav_v3/omninav"),
         "NAVIGATION" => render_static_partial(navigation_path),
         "PRIMARY CONTENT" => render_static_one_column_primary_content_business_grad,
         "GLOBAL FOOTER" => render_static_partial(footer_path),
@@ -156,7 +156,7 @@ module ContentTypes
         "PAGE WRAPPER OPEN" => cascade_format("_cascade/formats/modular/page_wrapper_open"),
 
         # TODO: convert these to cascade_format action.
-        "OMNI-NAV" => render_static_partial("widgets/shared/omninav"), #update this
+        "OMNI-NAV" => render_static_partial("omninav_v3/omninav"), #update this
         #'NAVIGATION' => render_static_partial(navigation_path), #remove this
         "PRIMARY CONTENT" => render_static_one_column_primary_content_subbrand,
         "GLOBAL FOOTER" => render_static_partial(footer_path),
@@ -188,7 +188,7 @@ module ContentTypes
         "PAGE WRAPPER OPEN" => cascade_format("_cascade/formats/modular/page_wrapper_open"),
 
         # TODO: convert these to cascade_format action.
-        "OMNI-NAV" => render_static_partial("widgets/shared/omninav"),
+        "OMNI-NAV" => render_static_partial("omninav_v3/omninav"),
         "NAVIGATION" => render_static_partial(navigation_path),
         "PRIMARY CONTENT" => render_static_one_column_primary_content_subbrand,
         "GLOBAL FOOTER" => render_static_partial(footer_path),
@@ -247,7 +247,7 @@ module ContentTypes
         "SOCIAL ACCOUNTS" => "TODO: _cascade/formats/level/social_accounts",
 
         # TODO: convert these to cascade_format action.
-        "OMNI-NAV" => render_static_partial("widgets/shared/omninav"),
+        "OMNI-NAV" => render_static_partial("omninav_v3/omninav"),
         "NAVIGATION" => render_static_partial(navigation_path),
         "GLOBAL FOOTER" => render_static_partial(footer_path),
       }
@@ -308,7 +308,7 @@ module ContentTypes
         # TODO: convert these to cascade_format action.
         "GLOBAL FOOTER" => render_static_partial("_cascade/blocks/html/footer"),
         "NAVIGATION" => render_static_partial("widgets/shared/navigation"),
-        "OMNI-NAV" => render_static_partial("widgets/shared/omninav"),
+        "OMNI-NAV" => render_static_partial("omninav_v3/omninav"),
       }
 
       render @configuration_set.template
@@ -357,7 +357,7 @@ module ContentTypes
         # TODO: convert these to cascade_format action.
         "GLOBAL FOOTER" => render_static_partial("_cascade/blocks/html/footer"),
         "NAVIGATION" => render_static_partial("widgets/shared/navigation"),
-        "OMNI-NAV" => render_static_partial("widgets/shared/omninav"),
+        "OMNI-NAV" => render_static_partial("omninav_v3/omninav"),
       }
 
       render @configuration_set.template
