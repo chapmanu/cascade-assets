7fc47c7044fcaa74de7d1c5435df75e813f42f1a
Chris <chrs.bryant@gmail.com>
2020-10-19 13:56:33 -0700

    added function for retrieving width for mobile scroll

--- app/javascript/src/hot-fixes/navigation-fixes.js
+++ app/javascript/src/hot-fixes/navigation-fixes.js
@@ -1,38 +1,23 @@
-// The function actually applying the offset
-// export const anchorOffset = () => {
-//   function offsetAnchor() {
-//     if (location.hash.length !== 0) {
-//       console.log( window.scrollY)
-//       // window.scrollTo(window.scrollX, window.scrollY + 120);
-//       console.log( window.scrollY)
-
-//     }
-//   }
-
-//   // Captures click events of all <a> elements with href starting with #
-//   $(document).on('click', 'a[href^="#"]', function(event) {
-//     // event.preventDefault()
-//     // Click events are captured before hashchanges. Timeout
-//     // causes offsetAnchor to be called after the page jump.
-//     window.setTimeout(function() {
-//       offsetAnchor();
-//     }, 0);
-//   });
-
-//   // Set the offset when entering page with hash present in the url
-//   window.setTimeout(offsetAnchor, 0);
-// }
 
 export const anchorOffset = () => {
-  // Desired offset, in pixels
-  var offset = 130;
-  // Desired time to scroll, in milliseconds
-  var scrollTime = 500;
+  function getWidth() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+  }
 
   $('a[href^="#"]').click(function() {
+    console.log('Width:  ' +  getWidth() );
+    const offset = getWidth() > 905 ? 131 : 71,
+    scrollTime = 250;
+
     // Need both `html` and `body` for full browser support
     $("html, body").animate({
-        scrollTop: $( $(this).attr("href") ).offset().top - offset 
+        scrollTop: $( $(this).attr("href") ).offset().top -  offset
     }, scrollTime);
 
     // Prevent the jump/flash
